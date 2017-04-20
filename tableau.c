84922621b1dc71ab588aa3492097e7e247e4bacb
Unknown <cedriclingom@rocketmail.com>
2017-04-20 18:26:49 +0200

    Add funcions tableau.c

--- tableau.c
+++ tableau.c
@@ -14,9 +14,13 @@
 
 
 /*------------------------------------------------------------------------------------------------------*/
+/* AllocationTableau            Alloue un tableau.                                                      */
 /*                                                                                                      */
-/*                                              tableau.h                                               */
-/*              
+/* En entrée             : taille - La taille du tableau.                                               */
+/*                                                                                                      */
+/* En sortie             : tab    - Pointeur sur un tableau alloué.                                     */
+/*                                                                                                      */
+/* Variable(s) locale(s) : tab    - Pointeur sur un tableau alloué.                                     */              
 /*                                                                                                      */
 /*------------------------------------------------------------------------------------------------------*/
 
@@ -30,4 +34,35 @@ nombre_t * AllocationTableau(int taille)
 }
 
 
-/*Afficher tableau*/
+/*------------------------------------------------------------------------------------------------------*/
+/* AfficherTableau            Affiche le contenu d'un tableau.                                          */
+/*                                                                                                      */
+/* En entrée             : taille - La taille du tableau.                                               */
+/*                         tab    - Pointeur sur le tableau à afficher.                                 */
+/*                                                                                                      */
+/* En sortie             :          Rien en sortie.                                                     */
+/*                                                                                                      */
+/* Variable(s) locale(s) : i      - Variable de boucle.                                                 */              
+/*                                                                                                      */
+/*------------------------------------------------------------------------------------------------------*/
+
+
+void AfficherTableau(nombre_t * tab, int taille)
+{
+
+  int i;
+
+  printf("Le contenu de mon tableau est:\n");
+
+  printf("[ ");
+
+  for(i = 0; i < taille - 1; ++i)
+    {
+
+      printf("%d, ", tab[i]);
+
+    }
+
+  printf("%d ]\n", tab[i]);
+
+}
--- tableau.h
+++ tableau.h
@@ -28,6 +28,8 @@
 
 nombre_t * AllocationTableau(int);
 
+void AfficherTableau(nombre_t *, int);
+
 
 
 
